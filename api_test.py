f778119f43ac9ded163f3e7528ab8adfd27c43b7
Willem Helf <sophie.helf@gmail.com>
2025-12-04 22:46:04 -0500

    cleaning dataframe successfully

--- api_test.py
+++ api_test.py
@@ -10,8 +10,16 @@ results = client.get("i7jb-7jku", limit=2000)
 results_df = pd.DataFrame.from_records(results)
 operational = results_df[results_df["status"] == "Operational"]
 
-columns_to_drop = ["location_type", "status", "changing_stations", "latitude", "longitude", "location_1", ":@computed_region_yeji_bk3q", ":@computed_region_sbqj_enih", ":@computed_region_92fq_4b7q", ":@computed_region_f5dn_yrer", "restroom_type", "open", "hours_of_operation", "website", "additional_notes"]
-final_df = results_df.drop(columns=columns_to_drop)
-final_df["accessibility"] = final_df["accessibility"].fillna("Not accessible")
+def clean_df(df):
+    columns_to_drop = ["location_type", "operator", "status", "changing_stations", "latitude", "longitude", "location_1", ":@computed_region_yeji_bk3q", ":@computed_region_sbqj_enih", ":@computed_region_92fq_4b7q", ":@computed_region_f5dn_yrer", "restroom_type", "website", "additional_notes"]
+    df = results_df.drop(columns=columns_to_drop)
+    df["accessibility"] = df["accessibility"].fillna("No")
+    df["hours_of_operation"] = df["hours_of_operation"].fillna("-")
+    df["open"] = df["open"].fillna("-")
+    df.rename(columns={'facility_name': 'Location',  "accessibility": 'Accessible?',  'open': 'Open', 'hours_of_operation': 'Hours'}, inplace=True)
+    return df
+
+final_df = clean_df(operational)
 print(final_df)
+
 #bathrooms_csv = operational.to_csv('bathrooms.csv', index=False)
